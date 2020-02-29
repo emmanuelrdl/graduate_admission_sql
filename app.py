a56509a8bf43960e1c41a2cd0fde54db2e6aef74
emmanuel rudelle <emmanuel.rudelle@hotmail.fr>
2020-02-29 13:26:29 +0100

    init porject

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template, redirect, url_for, request, make_response, jsonify
 import requests
+from sklearn.externals import joblib
+import numpy as np
 import json
 
 app = Flask(__name__)
 
-dark_sky_api_key = "073ed950bcd367ad35e76ea60cf5511c"
-ipstack_api_key ="55896dde6c19b26566166b446fe84094"
 
 @app.route("/")
 def form():
     response = make_response(render_template("form.html"))
     return response
 
-@app.route("/weather", methods = [ 'POST'])
-def weather():
-    lat = request.form['latitude']
-    long = request.form['longitude']
-    dark_sky = requests.get("https://api.darksky.net/forecast/{}/{},{}".format(dark_sky_api_key,lat,long), params={"lang":"fr"}).json()
+@app.route("/go", methods = [ 'POST'])
+def go():
+    experience = float(request.form['experience'])
+    model = joblib.load('linear_regression_model.pkl')
+    print(np.array([experience]).reshape(-1, 1))
+    prediction = model.predict(np.array([experience]).reshape(-1, 1))  
+    formatted_pred = round(prediction[0])
     response = make_response(render_template("index.html",
-                            forecast = dark_sky))
+                            forecast = formatted_pred))
     return response
 
 
